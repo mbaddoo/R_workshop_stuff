93c1c5d5e2cfc7e6b23b2a8fe4b5e136fd0325de
Melody Baddoo <mbaddoo@tulane.edu>
2015-01-27 13:02:41 -0600

    Combined country files

--- combine.sh
+++ combine.sh
@@ -7,14 +7,18 @@
 OUTPUT_FILE="output/combined_gapMinder.tsv"
 
 echo Building combined country data file
+# add column headers to file 
 cat data/clean/country.cc.txt > $OUTPUT_FILE
 
+# move file with column headers aside so it won't match wildcard
 mv -v data/clean/country.cc.txt data/clean/country.cc.processed
 sleep 2
 
 cat data/clean/*.cc.txt 
 sleep 2
+# everything that matches *.cc.txt, stick it on the bottom of outputfile (concatenate)
 cat data/clean/*.cc.txt >> $OUTPUT_FILE
 
+# move back to the way the country.cc.txt file was
 mv -v data/clean/country.cc.processed data/clean/country.cc.txt
 
